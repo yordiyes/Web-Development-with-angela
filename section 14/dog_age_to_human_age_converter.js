b90da995535d76b37a2061435b2def094c9e4fa2
Yordanos Yshitla <yordiman614@gmail.com>
2024-03-29 10:25:02 +0300

    some added js files

---
+++ section 14/dog_age_to_human_age_converter.js
@@ -0,0 +1,4 @@
+var equivHumanAge ;
+var dogAge = prompt("Enter Your Dog Age:");
+equivHumanAge = ((dogAge - 2)* 4) + 21;
+alert("You Dog Would Be "+ equivHumanAge+" Years Old If It Were A Human Being!")
