45963eb96d1ad609ffefcaf915ae0d58a29c21b2
Yordanos Yshitla <yordiman614@gmail.com>
2024-04-05 20:45:13 +0300

    my project on the simen game

--- section 20/game.js
+++ section 20/game.js
@@ -1,14 +1,13 @@
 var buttonColours = ["red", "blue", "green", "yellow" ];
+
 var gamePattern = [];
 var userClickedPattern = [];
 
 var started = false;
-
 var level = 0;
 
 $(document).keypress(function() {
   if (!started) {
-
     $("#level-title").text("Level " + level);
     nextSequence();
     started = true;
@@ -21,9 +20,12 @@ $(".btn").click(function (){
 
     playSound(userChosenColour);
     animatePress(userChosenColour);
+
+    checkAnswer(userClickedPattern.length - 1)
 });
 
 function nextSequence(){
+    userClickedPattern = [];
     level ++;
     $("#level-title").text("Level " + level);
     
@@ -43,10 +45,33 @@ function playSound(name){
 function animatePress(currentColour){
     $("#"+ currentColour).addClass("pressed")
     setTimeout(function() {
-    $("#"+ currentColour).removeClass('pressed');
+      $("#"+ currentColour).removeClass('pressed');
     }, 100);
 }
 
 function  checkAnswer(currentLevel){
-    
+    if(gamePattern[currentLevel] === userClickedPattern[currentLevel]){
+      console.log("Success");
+      if(userClickedPattern.length === gamePattern.length){
+        setTimeout(function (){
+          nextSequence();
+        }, 1000)
+      }
+    }else{
+      console.log("Wrong")
+      playSound("wrong");
+
+      $("body").addClass("game-over")
+      setTimeout(function(){
+        $("body").removeClass("game-over")
+      }, 200);
+
+      $("h1").text("Game Over, Press Any Key to Restart");
+      startOver();
+    }
+}
+function startOver(){
+  level = 0;
+  gamePattern = [];
+  started = false;
 }
