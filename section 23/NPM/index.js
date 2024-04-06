c1dbe7d13b327014b9a27a5bfb593c9e121cf513
Yordanos Yshitla <yordiman614@gmail.com>
2024-04-06 20:26:35 +0300

    about npm and node

---
+++ section 23/NPM/index.js
@@ -0,0 +1,11 @@
+// var generateName = require("sillyname");
+
+// import generateName from "sillyname" // this line and the first line do the same thing.
+// var firstName = generateName();
+// // this one is added by me
+// var lastName = generateName();
+// console.log(`My name is ${firstName} ${lastName}.`);
+
+import superheroes from "superheroes";
+var superHero = superheroes.random();
+console.log(`I am ${superHero}!`);
