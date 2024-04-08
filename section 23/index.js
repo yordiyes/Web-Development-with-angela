cf8db5a1c4044f882e5cdf0ed432bb3f5f4ad422
Yordanos Yshitla <yordiman614@gmail.com>
2024-04-08 21:12:30 +0300

    some changes

--- section 23/index.js
+++ section 23/index.js
@@ -2,3 +2,6 @@ console.log("This is to show that node reple actually works");
 for (var i=0; i < 5; i++){
     console.log(i);
 }
+
+// The reason why the above code works is because as we know the usage of js wass limited to the web browser till the birth of node js
+// and after it's birth js become more powerful and now we can even use node to deveope web apps, desktop app and cmd apps
