bdc8b46ba6efbcb5fbd242dfec54dec7080c5497
Yordanos Yshitla <yordiman614@gmail.com>
2024-04-12 17:50:12 +0300

    nodeman

--- section 24/M- Middleware/index3.js
+++ section 24/M- Middleware/index3.js
@@ -3,6 +3,11 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+function logger(req, res, next){
+  console.log("Request Method: ",req.method);
+  console.log("Reqeust URL: ", req.url);
+  next();
+}
 app.use(logger);
 
 app.get("/", (req, res) => {
--- section 24/M- Middleware/solution4.js
+++ section 24/M- Middleware/solution4.js
@@ -24,7 +24,7 @@ app.get("/", (req, res) => {
 });
 
 app.post("/submit", (req, res) => {
-  res.send(`<h1>Your band name is:</h1><h2>${bandName}✌️</h2>`);
+  res.send(`<h1>Your band name is:</h1><h2>${bandName} ✌️</h2>`);
 });
 
 app.listen(port, () => {
