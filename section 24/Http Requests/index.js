ad3cfb558f335d533ba3cb3fda8ef21eaae822f9
unknown <yordiman614@gmail.com>
2024-06-06 09:38:37 +0300

    deleting some unwanted files and creting new folders

--- section 24/Http Requests/index.js
+++ section 24/Http Requests/index.js
@@ -1,22 +1,13 @@
 import express from "express";
+
 const app = express();
 const port = 3000;
 
 app.get("/", (req, res)=>{
-    res.send("<h1>Hey Jordi how are you doing.</h1>");
     // console.log(req.rawHeaders);
-})
-
-app.get("/contact", (req, res)=>{
-    res.send("<h1>Contact Me</h1><p>Phone: +251 930393733</p>");
-    // console.log(req.rawHeaders);
-})
-
-app.get("/about", (req, res)=>{
-    res.send("<h1>About Me</h1><p>Hey there i am Yordanos</p>");
-    // console.log(req.rawHeaders);
+    res.send("<h1>This is to illustrate that the server that i have created is successfully listening on port 3000</h1><h2>Hey guys</h2>")
 })
 
 app.listen(port, ()=>{
-    console.log(`Server have been started on port ${port}.`)
+    console.log(`Server listening on port ${port}`);
 })
