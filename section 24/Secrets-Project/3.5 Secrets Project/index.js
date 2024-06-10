867bb440b9affcc4b09fac690ffa64e62db7b489
Yordanos Yeshitla <yordiman614@gmail.com>
2024-06-10 06:52:59 +0300

    final change

--- section 24/Secrets-Project/3.5 Secrets Project/index.js
+++ section 24/Secrets-Project/3.5 Secrets Project/index.js
@@ -9,14 +9,23 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
+var isAutorised = false;
 app.use(bodyParser.urlencoded({extended: true}));
+
+function passwordCheck(req, res,next){
+    let password = req.body.password;
+    if(password === "ILoveProgramming"){
+        isAutorised = true;
+    }next()
+}
+app.use(passwordCheck);
 
 app.get("/", (req,res)=>{
     res.sendFile(__dirname +"/public/index.html");
 })
 
 app.post("/check", (req, res)=>{
-    if(req.body.password === "ILoveProgramming"){
+    if(isAutorised){
        res.sendFile(__dirname + "/public/secret.html"); 
     } 
     else{
