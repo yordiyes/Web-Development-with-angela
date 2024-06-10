f164b9a0694f99a83e2f134e86f6e5337e49b64a
Yordanos Yeshitla <yordiman614@gmail.com>
2024-06-10 17:29:15 +0300

    changes

--- section 25/EJS Partials/index.js
+++ section 25/EJS Partials/index.js
@@ -4,22 +4,22 @@ const app = express();
 const port = 3000;
 
 // Write your code here:
-app.use(express.static("public"))
+app.use(express.static("public"));
 
 // Step 1: Render the home page "/" index.ejs
-app.use("/", (req, res)=>{
-  res.render("index.ejs")
-})
+app.get("/", (req, res)=>{
+  res.render("index.ejs");
+});
 // Step 2: Make sure that static files are linked to and the CSS shows up.
 // Step 3: Add the routes to handle the render of the about and contact pages.
-app.use("/about", (req, res)=>{
-  res.render("about.ejs")
-})
+app.get("/about", (req, res)=>{
+  res.render("about.ejs");
+});
 //   Hint: Check the nav bar in the header.ejs to see the button hrefs
 // Step 4: Add the partials to the about and contact pages to show the header and footer on those pages. 
-app.use("/contact", (req, res)=>{
-  res.render("contact.ejs")
-})
+app.get("/contact", (req, res)=>{
+  res.render("contact.ejs");
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
