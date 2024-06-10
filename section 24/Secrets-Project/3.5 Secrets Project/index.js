f0c679fc26066ea11f8f78f66ef7b7f89790a536
Yordanos Yeshitla <yordiman614@gmail.com>
2024-06-10 07:03:04 +0300

    ..

--- section 24/Secrets-Project/3.5 Secrets Project/index.js
+++ section 24/Secrets-Project/3.5 Secrets Project/index.js
@@ -10,19 +10,21 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 3000;
 var isAutorised = false;
+
 app.use(bodyParser.urlencoded({extended: true}));
 
 function passwordCheck(req, res,next){
     let password = req.body.password;
     if(password === "ILoveProgramming"){
         isAutorised = true;
-    }next()
+    }
+    next()
 }
 app.use(passwordCheck);
 
 app.get("/", (req,res)=>{
     res.sendFile(__dirname +"/public/index.html");
-})
+});
 
 app.post("/check", (req, res)=>{
     if(isAutorised){
@@ -31,7 +33,7 @@ app.post("/check", (req, res)=>{
     else{
         res.sendFile(__dirname +"/public/index.html");
     }
-})
+});
 
 app.listen(port, ()=>{
     console.log(`Server Listening On Port${port}.`)
