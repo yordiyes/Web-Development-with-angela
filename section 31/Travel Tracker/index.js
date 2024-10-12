ade20714d7951e78934de792339f4cd05d1f889f
Yordanos Yeshitla <yordiman614@gmail.com>
2024-10-12 20:50:13 +0300

    some changes that i have made about the tracker project

--- section 31/Travel Tracker/index.js
+++ section 31/Travel Tracker/index.js
@@ -17,18 +17,40 @@ db.connect();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get("/", async (req, res) => {
-  //Write your code here.
-  const result = await bd.query("SELECT country_code FROM visited_countries")
-  let countries = []
+async function checkVisited(){
+  const result = await db.query("SELECT country_code FROM visited_countries");
+
+  let countries = [];
   result.rows.forEach((country) =>{
     countries.push(country.country_code);
-  });
+  })
+  return countries;
+}
 
-  console.log(result.rows);
+app.get("/", async (req, res) => {
+  //Write your code here.
+  const countries = await checkVisited();
   res.render("index.ejs", {countries: countries, total: countries.length})
-  db.end();
+
 });
+
+app.post("/add", async (req, res) =>{
+  const input = req.body["country"];
+
+  const result = await db.query("SELECT country_code FROM countries WHERE country_name = $1", 
+    [input]
+  );
+  
+  if(result.rows.length !== 0){
+    const data = result.rows[0];
+    const countryCode = data.country_code;
+
+    await db.query("INSERT INTO visited_countries (country_code) VALUES ($1)", [
+      countryCode
+    ]);
+    res.redirect("/")
+  }
+})
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
--- section 31/Travel Tracker/solution1.js
+++ section 31/Travel Tracker/solution1.js
@@ -8,7 +8,7 @@ const port = 3000;
 const db = new pg.Client({
   user: "postgres",
   host: "localhost",
-  database: "world",
+  database: "World",
   password: "yopg123",
   port: 5432,
 });
