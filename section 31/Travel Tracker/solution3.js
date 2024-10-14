ffe63b2c438861d3dc3bb2b97ea760ceb4d6052a
Yordanos Yeshitla <yordiman614@gmail.com>
2024-10-14 14:36:32 +0300

    miner change

--- section 31/Travel Tracker/solution3.js
+++ section 31/Travel Tracker/solution3.js
@@ -8,8 +8,8 @@ const port = 3000;
 const db = new pg.Client({
   user: "postgres",
   host: "localhost",
-  database: "world",
-  password: "123456",
+  database: "World",
+  password: "yopg123",
   port: 5432,
 });
 db.connect();
