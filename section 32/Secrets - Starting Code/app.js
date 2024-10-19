31ba121073468cb53326ff8d986aceaf212252cb
Yordanos Yeshitla <yordiman614@gmail.com>
2024-10-19 22:09:59 +0300

    using environmental variables for encryption

--- section 32/Secrets - Starting Code/app.js
+++ section 32/Secrets - Starting Code/app.js
@@ -1,3 +1,6 @@
+require("dotenv").config();
+console.log(process.env.SECRET); // Ensure the secret is loaded
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -25,8 +28,7 @@ const userSchema = new mongoose.Schema({
     password: String
 })
 
-const secret = "Thisisourlittlesecret.";
-userSchema.plugin(encrypt, {secret: secret, encryptedFields: ["password"] })
+userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"] })
 
 const user = new mongoose.model("User", userSchema);
 
